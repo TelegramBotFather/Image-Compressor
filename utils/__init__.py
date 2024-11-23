60e94a0dd56664255e8325911d5714000a5ff5ae
Md Matin Ashraf <91468485+Ashrafmdmatin41@users.noreply.github.com>
2024-11-23 12:30:14 +0530

    c

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,4 @@
 from .helpers import (
-    get_image_info,
-    download_image,
     clean_temp_files,
     format_size,
     cleanup_old_data
@@ -9,8 +7,6 @@ from .decorators import admin_only, rate_limit, handle_errors
 from .error_handler import handle_error
 
 __all__ = [
-    'get_image_info',
-    'download_image',
     'clean_temp_files',
     'format_size',
     'cleanup_old_data',
